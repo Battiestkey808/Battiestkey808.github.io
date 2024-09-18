592cdbd25ef1f32836a21044b6070c0fed8e9d21
Battiestkey808 <evanlouie808@gmail.com>
2024-09-17 22:30:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const closeBtn = document.querySelector('.close-btn');
 
     hamburger.addEventListener('click', function() {
-        sidebar.classList.add('show'); // Show the sidebar
+        sidebar.style.left = '0'; // Show the sidebar
     });
 
     closeBtn.addEventListener('click', function() {
-        sidebar.classList.remove('show'); // Hide the sidebar
+        sidebar.style.left = '-250px'; // Hide the sidebar
     });
 });
